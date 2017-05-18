ffc9944d369171fe0a3414f670191ef8392c7fe7
d.zenin <zenin1@rambler.ru>
2017-05-18 12:34:27 +0300

    Using RSS-channel Atom

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :orders
   resources :line_items
   resources :carts
-  get 'store/index'
+  get 'store/index', as: 'store_index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
     # The priority is based upon order of creation:
     # (Приоритет основан на порядке создания:)
     # first created -> highest priority.
@@ -12,7 +14,7 @@ Rails.application.routes.draw do
     # See how all your routes lay out with "rake routes".
     # (Раскладку всех маршрутов можно увидеть с помощью команды "rake routes".)
     # You can have the root of your site routed with "root"
-    # (Корневой маршрут к вашему сайту можно получить с помощью "root") 
-   root to: 'store#index', as: 'store'
+    # (Корневой маршрут к вашему сайту можно получить с помощью "root")
+  root to: 'store#index', as: 'store'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
